c91b1b2f5ddf29fbc1ecb869cc023abbd4613788
Nova <115679769+pc20211063@users.noreply.github.com>
2022-10-13 11:18:12 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,57 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(randint(1, 6))
 })
-basic.forever(function () {
-	
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(9)
+    basic.showNumber(8)
+    basic.showNumber(7)
+    basic.showNumber(6)
+    basic.showNumber(5)
+    basic.showNumber(4)
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showNumber(0)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
